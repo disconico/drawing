ca7ff6bb25684ada235f0038485785b5af583519
Nico <disconico@outlook.com>
2022-05-12 15:49:20 +0200

    Update files, painting not finished yet

--- scripts.js
+++ scripts.js
@@ -1,18 +1,43 @@
 //Assign all elements
-const container = document.getElementsByClassName('main-container');
+const sizeButton = document.querySelectorAll('.size-button');
+const container = document.querySelectorAll('.main-container');
+const blocks = document.querySelectorAll('.square-div');
 
-//Create div function
+//Get the player input and set the divs
+sizeButton.forEach(button => button.addEventListener('click', function(e){
+    clear ();
+    userInput = e.target.id;
+    // console.log(userInput);
+    createDiv(userInput); //create n divs = button pushed #
+    // console.log(blocks);
+    console.log(document.querySelectorAll('.square-div'));
+}))
+
+//Create the square divs
 function createDiv (userInput) {
     for (let i = 0; i < userInput; i++) {
         const newDiv = document.createElement('div');
         newDiv.setAttribute("class", "square-div");
         newDiv.setAttribute("id", `Square #${i}`);
-        newDiv.appendChild(document.createTextNode(`I'm the div ${i+1}`));
+        newDiv.setAttribute('tabindex',`${i}`);
+        newDiv.appendChild(document.createTextNode(``));
+        let squareSize = Math.sqrt((160000 / userInput));
+        let roundedSquareSize = Math.round(squareSize * 100) / 100;
+        newDiv.style.width = roundedSquareSize + 'px';
+        newDiv.style.height = roundedSquareSize + 'px';
         container[0].appendChild(newDiv);
     }
 }
 
+//Clear function
+function clear () {
+    const bigBox = document.getElementById("big-box");
+    bigBox.innerHTML = ''
+}
 
+//Color on hover
+blocks.forEach(block => block.addEventListener('click', function (e){
+    console.log(e.target);
+    e.target.style.backgroundColor = "black";
+}))
 
-//Go
-createDiv(16);
